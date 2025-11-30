c171a8025c0c5773fe66d49dcd90f7fd8bfe6ad8
ilya <ilya@andrianov.ru>
2025-11-30 03:30:22 +0300

    Добавлена задача с простыми числами

---
+++ PrimeNumbers/PrimeNumbers.Tests/PrimeTests.fs
@@ -0,0 +1,16 @@
+﻿module PrimeNumbers.Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit.MsTest
+open PrimeNumbers
+
+[<TestClass>]
+type PrimeTests () =
+
+    [<TestMethod>]
+    member _.``The first element is two`` () =
+        primeNumbers () |> Seq.item 0 |> should equal 2
+
+    [<TestMethod>]
+    member _.``The tenth element is 29`` () =
+        primeNumbers () |> Seq.item 9 |> should equal 29
---
+++ PrimeNumbers/PrimeNumbers/PrimeNumbers.fs
@@ -0,0 +1,25 @@
+﻿module PrimeNumbers
+
+let isPrime n =
+    if n < 2 then false
+    else
+        let root = int (sqrt (float n))
+        let rec isPrimeHelper t =
+            if t > root then true
+            elif n % t = 0 then false
+            else isPrimeHelper (t + 1)
+        isPrimeHelper 2
+
+let primeNumberOfIndex n =
+    let rec primeNumberOfIndexHelper t ans =
+        if isPrime ans then
+            if t = n then ans
+            else primeNumberOfIndexHelper (t + 1) (ans + 1)
+        else primeNumberOfIndexHelper t (ans + 1)
+    primeNumberOfIndexHelper 0 2
+
+let primeNumbers () =
+        Seq.initInfinite primeNumberOfIndex
+
+let aboba = primeNumbers () |> Seq.take 10 |> Seq.toList
+printfn "%A" aboba
