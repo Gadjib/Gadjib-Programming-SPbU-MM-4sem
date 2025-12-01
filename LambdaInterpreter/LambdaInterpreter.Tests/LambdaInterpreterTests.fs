1e4594d9b16d37b3ef5c59f20712169cf45a3c63
ilya <ilya@andrianov.ru>
2025-12-01 19:46:45 +0300

    Добавлен интерпретатор лямбды

---
+++ LambdaInterpreter/LambdaInterpreter.Tests/LambdaInterpreterTests.fs
@@ -0,0 +1,84 @@
+﻿module LambdaInterpreter.Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit.MsTest
+open LambdaInterpreter
+
+[<TestClass>]
+type LambdaInterpreterTests () =
+
+    [<TestMethod>]
+    member _.``freeVars of variable is itself`` () =
+        freeVars (Var "x") |> should equal (set ["x"])
+
+    [<TestMethod>]
+    member _.``freeVars of abstraction removes bound var`` () =
+        freeVars (Abs ("x", Var "x")) |> should equal Set.empty<string>
+
+    [<TestMethod>]
+    member _.``freeVars of abstraction keeps only free vars`` () =
+        let expr = Abs ("x", App (Var "x", Var "y")) 
+        freeVars expr |> should equal (set ["y"])
+
+    [<TestMethod>]
+    member _.``freeVars of application is union of both sides`` () =
+        let expr = App (Abs ("x", Var "x"), Var "y")
+        freeVars expr |> should equal (set ["y"])
+
+    [<TestMethod>]
+    member _.``newName skips used names with index`` () =
+        let used = set ["x0"; "x1"; "y"]
+        let fresh = newName "x" used
+        fresh |> should equal "x2"
+
+    [<TestMethod>]
+    member _.``substitude replaces free occurrences of variable`` () =
+        let expr = App (Var "x", Var "y")          
+        let t = Abs ("z", Var "z")             
+        let result = substitude "x" t expr
+        result |> should equal (App (t, Var "y"))
+
+    [<TestMethod>]
+    member _.``substitude does nothing if variable not present`` () =
+        let expr = Abs ("y", App (Var "y", Var "z"))
+        let t = Var "x"
+        substitude "w" t expr |> should equal expr
+
+    [<TestMethod>]
+    member _.``betaReductionStep reduces simple beta-redex`` () =
+        let expr = App (Abs ("x", Var "x"), Var "y")    
+        let result = betaReductionStep expr
+        result |> should equal (Some (Var "y"))
+
+    [<TestMethod>]
+    member _.``betaReductionStep reduces leftmost outermost redex`` () =
+        let expr =
+            App (
+                App (Abs ("x", Var "x"), Var "y"),
+                App (Abs ("z", Var "z"), Var "w")
+            )
+        let expected =
+            App (
+                Var "y",
+                App (Abs ("z", Var "z"), Var "w")
+            )
+        betaReductionStep expr |> should equal (Some expected)
+
+    [<TestMethod>]
+    member _.``betaReduction reduces identity application`` () =
+        let expr = App (Abs ("x", Var "x"), Var "y")
+        betaReduction expr |> should equal (Var "y")
+
+    [<TestMethod>]
+    member _.``betaReduction with nested abstractions and alpha conversion`` () =
+        let expr =
+            App (
+                Abs ("x", Abs ("y", Var "x")),
+                Var "y"
+            )
+        let result = betaReduction expr
+        match result with
+        | Abs (yName, body) ->
+            yName |> should equal "y0"
+            body  |> should equal (Var "y")
+        | _ -> Assert.Fail "Expected abstraction"
---
+++ LambdaInterpreter/LambdaInterpreter/LambdaInterpreter.fs
@@ -0,0 +1,58 @@
+﻿module LambdaInterpreter
+
+type Expr = 
+    | Var of string
+    | App of Expr * Expr
+    | Abs of string * Expr
+
+let rec freeVars expr =
+    match expr with
+    | Var x -> set [x]
+    | App (s, t) -> Set.union (freeVars s) (freeVars t)
+    | Abs (x, s) -> Set.remove x (freeVars s)
+
+let newName old (used: Set<string>) =
+    let rec newNameHelper i =
+        let t = old + string i
+        if used.Contains t then newNameHelper (i + 1)
+        else t
+    in
+    newNameHelper 0
+
+let rec substitude x t s =
+    match s with
+    | Var y -> if y = x then t else s
+    | App (s1, s2) -> App (substitude x t s1, substitude x t s2)
+    | Abs (y, s1) -> 
+        if y = x then s
+        else
+            let freeVarsT = freeVars t in
+            let freeVarsS = freeVars s1 in
+            if not (freeVarsT.Contains y) || not (freeVarsS.Contains x) then
+                Abs (y, substitude x t s1)
+            else
+                let used = Set.unionMany [ freeVarsT; freeVarsS ]
+                let z = newName y used
+                Abs (z, substitude x t (substitude y (Var z) s1))
+
+
+let rec betaReductionStep expr =
+    match expr with
+    | App (Abs (x, s), t) -> Some (substitude x t s)
+    | App (s, u) -> 
+        match betaReductionStep s with
+        | Some s1 -> Some (App (s1, u))
+        | None -> 
+            match betaReductionStep u with
+            | Some u1 -> Some (App (s, u1))
+            | None -> None
+    | Abs (x, s) ->
+        match betaReductionStep s with
+        | Some t -> Some (Abs (x, t))
+        | None -> None
+    | Var x -> None
+in
+let rec betaReduction expr =
+    match betaReductionStep expr with
+    | Some expr1 -> betaReduction expr1
+    | None -> expr
